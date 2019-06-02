cb968394bdf5026e7a750c8f80dbfa544c80735b
Jordan Castelloe <jordan.castelloe@gmail.com>
2019-06-02 14:50:35 -0400

    scaffolded controllers for each resource, embedded student links in instructor detail view

---
+++ StudentExercisesEF2/Migrations/20190602180558_isComplete.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentExercisesEF.Migrations
+{
+    public partial class isComplete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isComplete",
+                table: "StudentExercise",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isComplete",
+                table: "StudentExercise");
+        }
+    }
+}
