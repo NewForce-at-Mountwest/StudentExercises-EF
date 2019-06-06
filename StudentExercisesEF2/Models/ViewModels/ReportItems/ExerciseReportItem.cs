8b35f562b19d0f57a2883ce32831980deecfae20
Jordan Castelloe <jordan.castelloe@gmail.com>
2019-06-06 05:54:08 -0400

    added partial views for report

--- StudentExercisesEF2/Models/ViewModels/ReportItems/ExerciseReportItem.cs
+++ StudentExercisesEF2/Models/ViewModels/ReportItems/ExerciseReportItem.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace StudentExercisesEF.Models.ViewModels.ReportItems
 {
-    public class ExerciseReportItem
+    public class ExerciseReportItem 
     {
-
+        [Display(Name="Exercise")]
         public Exercise exercise { get; set; }
+
+        [Display(Name ="Number of Students")]
         public int numberOfStudents { get; set; }
 
     }
--- StudentExercisesEF2/Models/ViewModels/ReportItems/StudentReportItem.cs
+++ StudentExercisesEF2/Models/ViewModels/ReportItems/StudentReportItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,10 @@ namespace StudentExercisesEF.Models.ViewModels.ReportItems
 {
     public class StudentReportItem
     {
+        [Display(Name="Student")]
         public Student student { get; set; }
+
+        [Display(Name ="Number of Exercises")]
         public int numberOfExercises { get; set; }
     }
 }
